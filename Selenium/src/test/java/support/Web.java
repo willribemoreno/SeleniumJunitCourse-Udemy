416c38af1ebd58461954484da87f23bf61d2a44d
willribemoreno <66268223+willribemoreno@users.noreply.github.com>
2020-09-10 16:18:06 -0300

    Exercise finalized + Cloud

--- Selenium/src/test/java/support/Web.java
+++ Selenium/src/test/java/support/Web.java
@@ -1,11 +1,19 @@
 package support;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class Web {
+	
+	public static final String USERNAME = "williamribeiro1";
+	public static final String AUTOMATE_KEY = "pBhpDDqPZ42QajuZyzqa";
+	public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
 	
 public static WebDriver chromeInstance() {
 		
@@ -17,6 +25,32 @@ public static WebDriver chromeInstance() {
 		
 		//navegando para a página do taskit
 		navegador.get("http://www.juliodelima.com.br/taskit");	
+		
+		return navegador;
+	}
+
+	public static WebDriver createBrowserStack() {
+		
+		DesiredCapabilities caps = new DesiredCapabilities();
+        
+		caps.setCapability("os", "Windows");
+		caps.setCapability("os_version", "10");
+		caps.setCapability("browser", "Chrome");
+		caps.setCapability("browser_version", "80");
+		caps.setCapability("name", "williamribeiro1's First Test");
+		
+		WebDriver navegador = null;
+		
+		try {
+			navegador = new RemoteWebDriver(new URL(URL), caps);
+			navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+			
+			//navegando para a página do taskit
+			navegador.get("http://www.juliodelima.com.br/taskit");	
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("There is a problem on your URL: " + e.getMessage());
+		}
 		
 		return navegador;
 	}
--- Selenium/src/test/java/tests/SecondInformacoesUsuarioPageObjectTest.java
+++ Selenium/src/test/java/tests/SecondInformacoesUsuarioPageObjectTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.openqa.selenium.WebDriver;
@@ -18,10 +19,14 @@ public class SecondInformacoesUsuarioPageObjectTest {
 	
 	@BeforeEach
 	public void setUp() {
+//		rodar os testes offline
 		navegador = Web.chromeInstance();
+//		rodar os testes na nuvem
+//		navegador = Web.createBrowserStack();
 	}
 	
 //	@Test
+	@DisplayName("Testing taskit page")
 	@ParameterizedTest(name = "{index} => type = {0}, contact = {1}, message = {2}")
 	@CsvFileSource(resources = "/InformacoesUsuarioInputData.csv")
 
